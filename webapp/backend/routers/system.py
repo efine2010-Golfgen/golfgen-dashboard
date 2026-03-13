a29af810f22a04601e3bcfcbacce64e0e3034498
Eric Fine <efine2010@gmail.com>
2026-03-13 14:19:49 -0500

    Add SSO debug info to health endpoint

--- webapp/backend/routers/system.py
+++ webapp/backend/routers/system.py
@@ -42,6 +42,8 @@ def health():
         },
         "last_sync": {},
         "has_sp_api_creds": _load_sp_api_credentials() is not None,
+        "has_google_sso_creds": bool(os.environ.get("GOOGLE_CLIENT_ID")) and bool(os.environ.get("GOOGLE_CLIENT_SECRET")),
+        "google_redirect_uri_set": bool(os.environ.get("GOOGLE_REDIRECT_URI")),
     }
 
     try:
